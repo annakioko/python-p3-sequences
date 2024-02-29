3c8db0a37ff4c85cea753a8afa9ba8ff3afbb0b6
Anna Kioko <annakioko00@gmail.com>
2024-02-29 03:39:25 +0300

    completed assignment

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,17 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fib_sequence = []
+    if length == 0:
+        print ([])
+    elif length == 1:
+        print ([0])
+    elif length == 2:
+         print ([0, 1])
+    else:
+        fib_sequence = [0,1]
+        for _ in range(2, length):
+            next_fib = fib_sequence[-1] + fib_sequence[-2]  
+            fib_sequence.append(next_fib)  
+        print(fib_sequence)
+          
